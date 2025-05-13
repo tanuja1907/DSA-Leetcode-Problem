ce3c261c150a1f5d623a21cbff81f6abb4127c20
tanuja1907 <me.tanujamehra12@gmail.com>
2025-05-13 12:15:39 +0530

    Day 8 problem

--- Day-8/src/Problem_1_Lowest_Common_Ancestor_Of_BST/Solution.java
+++ Day-8/src/Problem_1_Lowest_Common_Ancestor_Of_BST/Solution.java
@@ -1,4 +1,49 @@
 package Problem_1_Lowest_Common_Ancestor_Of_BST;
 
+
+/**
+ *this program to find  lowest common ancestor   between two nodes p and q as the lowest node in T ,
+ * that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ * T.C.-O(H)
+ * S.C.-O(H)
+ */
 public class Solution {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        TreeNode p = root.left;
+        TreeNode q = root.right;
+        TreeNode lca = solution.lowestCommonAncestor(root, p, q);
+        System.out.println("LCA of " + p.val + " and " + q.val + " is: " + lca.val);
+
+    }
 }
