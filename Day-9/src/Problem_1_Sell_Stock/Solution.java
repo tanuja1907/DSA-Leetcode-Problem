049f1054b74b7c75ee0aaa8ed159c73928df5c62
tanuja1907 <me.tanujamehra12@gmail.com>
2025-05-13 12:54:50 +0530

    Day 9 problem

--- Day-9/src/Problem_1_Sell_Stock/Solution.java
+++ Day-9/src/Problem_1_Sell_Stock/Solution.java
@@ -1,4 +1,26 @@
 package Problem_1_Sell_Stock;
 
+/**
+ * T.C.-O(n)
+ * S.C.-O(1)
+ */
 public class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            }
+            maxProfit = Math.max(maxProfit, prices[i] - buy);
+        }
+        return maxProfit;
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        int[] stockPrizes={7,6,4,3,1};
+        System.out.println("Max profit earn is "+ solution.maxProfit(stockPrizes));
+    }
 }
--- Day-9/src/Problem_2_Group_Anagrams/Solution.java
+++ Day-9/src/Problem_2_Group_Anagrams/Solution.java
@@ -1,4 +1,28 @@
 package Problem_2_Group_Anagrams;
 
-public class Solution {
+import java.util.*;
+
+/**
+ * S.C.-O(n*k)
+ * T.C.-O(n*k log k)
+ */
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        Solution solution = new Solution();
+        List<List<String>> result = solution.groupAnagrams(words);
+        System.out.println(result);
+    }
 }
