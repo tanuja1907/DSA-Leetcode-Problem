76736fea5887db4ca553d9da0dd5d0299be91ea0
tanuja1907 <me.tanujamehra12@gmail.com>
2025-05-13 15:08:51 +0530

    Day 7 problem

--- Day-7/src/Problem_2_Validate_Binary_Search_Tree/Solution.java
+++ Day-7/src/Problem_2_Validate_Binary_Search_Tree/Solution.java
@@ -1,4 +1,41 @@
 package Problem_2_Validate_Binary_Search_Tree;
 
-public class Solution {
+/**
+ * T.C.-O(n)
+ * S.C.-O(h),h is height of tree
+ */
+class Solution {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    boolean isValid(TreeNode node, long min, long max) {
+        if (node == null) return true;
+        if (node.val <= min || node.val >= max) return false;
+        return isValid(node.left, min, node.val) && isValid(node.right, node.val, max);
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        Solution solution = new Solution();
+        System.out.println(solution.isValidBST(root) + ", this tree is valid binary search tree!.");
+    }
 }
--- Day-7/src/Problem_3_ReorderList/Solution.java
+++
@@ -1,4 +0,0 @@
-package Problem_3_ReorderList;
-
-public class Solution {
-}
